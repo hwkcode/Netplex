65b17dcac2e50f6656cdcd7a4d62fb062ced4b2d
hwkcode <hwkcode@gmail.com>
2022-02-27 02:56:19 -0500

    Add app and root components

--- frontend/netplex.jsx
+++ frontend/netplex.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import configureStore from "./store/store";
+import Root from "./components/root";
 import { signup, login, logout } from '../frontend/util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Create something you're proud of!</h1>, root);
-    
-    // test
+    ReactDOM.render(<Root store={store} />, root);
+
+     // TESTING START
     window.login = login;
     window.logout = logout;
     window.signup = signup;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+    // TESTING END
+    
 });
