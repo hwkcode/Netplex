58c58a75f68cdad231b3e07d215fc0e41d88fd28
hwkcode <hwkcode@gmail.com>
2022-04-21 18:22:41 -0400

    Add profile name to avatar

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,13 +5,13 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="contactLinks">
-                <a href="https://www.linkedin.com/in/jonathankimmbapmp/" target="_blank" id="link">
+                <a href="https://www.linkedin.com/in/jonathankimmbapmp/" target="_blank" rel="noopener noreferrer" id="link">
                     <FontAwesomeIcon icon="fa-brands fa-linkedin-in" size='3x' className='contact-icons'/>
                 </a>
-                <a href="https://github.com/hwkcode" target="_blank" id="link">
+                <a href="https://github.com/hwkcode" target="_blank" rel="noopener noreferrer" id="link">
                     <FontAwesomeIcon icon="fa-brands fa-github-alt" size='3x' className='contact-icons' />
                 </a>
-                <a href="https://angel.co/u/jonathan-kim-mba-pmp" target="_blank" id="link">
+                <a href="https://angel.co/u/jonathan-kim-mba-pmp" target="_blank" rel="noopener noreferrer" id="link">
                     <FontAwesomeIcon icon="fa-brands fa-angellist" size='3x' className='contact-icons' />
                 </a>
             </div>
